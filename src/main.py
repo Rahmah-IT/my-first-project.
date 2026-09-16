f5a2b48b46b86cedfd7e83338e4e55b7beba2f32
Rahmah Alghamdi <ralghamd@marymount.edu>
2026-09-16 16:06:17 -0400

    completed sprint 1.

---
+++ src/main.py
@@ -0,0 +1,9 @@
+"""Serves as the point of entry to the point of entry to the Robot Rat Applicaion."""
+
+from robotrat_app import RobotRatApp
+
+def main():
+    robot_rat_app = RobotRatApp()
+
+if __name__ == '__main__':
+    main()    
